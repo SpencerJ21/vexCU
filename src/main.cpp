92070924186807a228a59bc575c6979780cd9203
SpencerJ <spencerj21@chcstudent.com>
2020-09-14 09:12:57 -0400

    misc changes and updates

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ void arrPrint(const std::array<double,N> &values, char id) {
     std::cout << i << ", ";
   }
 
-  std::cout << "0\n";
+  std::cout << "0";
 }
 
 void initialize() {
@@ -34,14 +34,35 @@ std::shared_ptr<OdomInput4Imu> odom5;
 
 void opcontrol() {
 
-  auto lEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(1,2), M_PI * 2.75 / 360.0);
-  auto rEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(3,4), M_PI * 2.75 / 360.0);
-  auto bEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(5,6), M_PI * 2.75 / 360.0);
-  auto fEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(7,8), M_PI * 2.75 / 360.0);
-  auto imu  = std::make_shared<kappa::ImuInput>(1);
+  auto lEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(3,4), -M_PI * 2.75 / 360.0);
+  auto rEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(5,6),  M_PI * 2.75 / 360.0);
+  auto bEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(7,8), -M_PI * 2.75 / 360.0);
+  auto fEnc = std::make_shared<kappa::OkapiInput>(std::make_shared<okapi::ADIEncoder>(1,2),  M_PI * 2.75 / 360.0);
+  auto imu  = std::make_shared<kappa::ImuInput>(15);
+
+  auto c1 = lv_chart_create(lv_scr_act(), NULL);
+  lv_obj_set_size(c1, 480, 240);
+  lv_obj_set_pos(c1, 0, 0);
+  lv_chart_set_type(c1, LV_CHART_TYPE_LINE);
+  lv_chart_set_range(c1, -90, 90);
+  lv_chart_set_point_count(c1, 240);
+  auto s1 = lv_chart_add_series(c1, LV_COLOR_BLUE);
+  auto s2 = lv_chart_add_series(c1, LV_COLOR_RED);
+
+  auto filterImu = 
+    std::make_shared<kappa::InputChartLogger<double>>(c1,s2,
+      std::make_shared<kappa::InputFilter<double>>(std::make_unique<okapi::EKFFilter>(0.01, 0.023879594),
+        std::make_shared<kappa::InputChartLogger<double>>(c1,s1,
+          imu
+        )
+      )
+    );
+
+  imu->calibrate();
+  pros::delay(5000);
 
   odom1 =
-    std::make_shared<OdomInput>(OdomInput::OdomVals{10, 5, 0.005},
+    std::make_shared<OdomInput>(OdomInput::OdomVals{13.425, 5.45, 0.01},
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
@@ -51,7 +72,7 @@ void opcontrol() {
     );
 
   odom2 =
-    std::make_shared<OdomInput4>(OdomInput4::OdomVals{10, 10, 0.005},
+    std::make_shared<OdomInput4>(OdomInput4::OdomVals{13.425, 10.906, 0.01},
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
@@ -61,7 +82,7 @@ void opcontrol() {
     );
 
   odom3 =
-    std::make_shared<OdomInput2Imu>(OdomInput2Imu::OdomVals{5, 5, 0.005},
+    std::make_shared<OdomInput2Imu>(OdomInput2Imu::OdomVals{6.7126, 5.45, 0.01},
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
@@ -71,7 +92,7 @@ void opcontrol() {
     );
 
   odom4 =
-    std::make_shared<OdomInput3Imu>(OdomInput3Imu::OdomVals{10, 5, 0.005},
+    std::make_shared<OdomInput3Imu>(OdomInput3Imu::OdomVals{13.425, 5.45, 0.01},
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
@@ -81,7 +102,7 @@ void opcontrol() {
     );
 
   odom5 =
-    std::make_shared<OdomInput4Imu>(OdomInput4Imu::OdomVals{10, 10, 0.005},
+    std::make_shared<OdomInput4Imu>(OdomInput4Imu::OdomVals{13.425, 10.906, 0.01},
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
       std::make_unique<okapi::PassthroughFilter>(),
@@ -91,7 +112,6 @@ void opcontrol() {
     );
 
   pros::Task testController([&] {
-    std::uint32_t now = pros::millis();
 
     while (true) {
       arrPrint(odom1->step(), '1');
@@ -99,9 +119,10 @@ void opcontrol() {
       arrPrint(odom3->step(), '3');
       arrPrint(odom4->step(), '4');
       arrPrint(odom5->step(), '5');
+
       std::cout << "\n";
 
-      pros::Task::delay_until(&now, 5);
+      pros::delay(10);
     }
 
   }, "Test Controller");
