6c797387b04d1e135fb498a6e7884ee4e1553c48
spencerj <spencerj21@chcstudent.com>
2021-02-17 14:01:04 -0500

    update kappaFramework and setup chassis motors

--- src/robot.cpp
+++ src/robot.cpp
@@ -12,7 +12,10 @@ void initialize(){
 
   robot::chassis = std::make_shared<kappa::ThreeAxisChassis>(4.2, 17,
     std::make_shared<kappa::ArrayDistributor<double,4>>(std::initializer_list<std::shared_ptr<kappa::AbstractOutput<double>>>{
-      // motors with vpid
+      std::make_shared<kappa::VPidSubController>(kappa::makeVPIDMotor(0, {0,0,50,620})),
+      std::make_shared<kappa::VPidSubController>(kappa::makeVPIDMotor(0, {0,0,50,620})),
+      std::make_shared<kappa::VPidSubController>(kappa::makeVPIDMotor(0, {0,0,50,620})),
+      std::make_shared<kappa::VPidSubController>(kappa::makeVPIDMotor(0, {0,0,50,620}))
     })
   );
 
